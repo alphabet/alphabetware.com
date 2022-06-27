32988d558635bac6d2d1abdb31982994d7207174
Gui Weinmann <gui.weinmann@consumer.org>
2022-06-27 12:43:41 -0400

    Change Auth Token

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,6 +1,6 @@
 
 	  # twilio configuration params, these should be environment variables
-		ACCOUNT_SID= ENV['TWILIO_ACCOUNT_SID']
+	  ACCOUNT_SID= ENV['TWILIO_ACCOUNT_SID']
 	  AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
 	  FROM = ENV['TWILIO_FROM_NUMBER'] # Your Twilio number
 
