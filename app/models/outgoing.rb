613577f817dd1652c87f8551060feaefb15d15cc
Gui Weinmann <gui.weinmann@consumer.org>
2022-06-28 23:22:50 -0400

    Add MS SID for Twilio

--- app/models/outgoing.rb
+++ app/models/outgoing.rb
@@ -1,4 +1,4 @@
-class Outgoing < Message 
+class Outgoing < Message
 	validates :body, presence: true
 	validate :from_cannot_be_same_as_to
 	before_validation :set_from_phone
@@ -9,13 +9,16 @@ class Outgoing < Message
 	end
 
 	def send_twilio_message
-		client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN 
+
+		client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 		if self.valid?
-			outgoing = client.account.messages.create(:from => from_phone, 
-																								:to => to_phone, 
-																								:body => body)
+			outgoing = client.messages.create(
+			:from => from_phone,
+            :to => to_phone,
+            :body => body,
+            :messaging_service_sid => MESSAGING_SERVICE_SID)
 			self.sms_sid = outgoing.sid
-			self.account_sid = outgoing.account_sid 
+			self.account_sid = outgoing.account_sid
 			self.twilio_api_version = outgoing.api_version
 			self.sent_at = Time.now
 		end
@@ -26,7 +29,7 @@ class Outgoing < Message
 		if to_phone == FROM
 			errors.add(:to_phone, "Message to is the same as #{FROM}")
 		end
-		if to_phone == from_phone 
+		if to_phone == from_phone
 			errors.add(:from_phone, "Message to is the same as Message from (#{from_phone})")
 		end
 	end
